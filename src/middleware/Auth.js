6bd257bb4126b90faebb753210595cb6ceba1359
atharmohammad <mohd.rule123@gmail.com>
2021-05-27 15:35:30 +0530

    Added user authentication and signup

---
+++ src/middleware/Auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const Users = require("../models/User");
+const mongoose = require("mongoose");
+
+const auth = async(req,res,next)=>{
+  try{
+    const token = req.header("Authorization").split(" ")[1];
+    const decodedKey = jwt.verify(token,"Random-Secret");
+
+    const user = await Users.findOne({_id:decodedKey._id,
+                              'tokens.token':token});
+
+    if(!user)
+      throw new Error();
+
+    req.token = token;
+    req.user = user;
+    next();
+  }catch(e){
+    res.status(401).send({'error':'Please Authenticate !'});
+  }
+}
+
+module.exports = auth;
