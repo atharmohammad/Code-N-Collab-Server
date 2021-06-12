ab10ef542924e95b6ef00eba1291295be189381f
atharmohammad <mohd.rule123@gmail.com>
2021-06-12 18:59:46 +0530

    flexible fields

--- src/models/User.js
+++ src/models/User.js
@@ -46,8 +46,8 @@ const userSchema = new mongoose.Schema(
       type: String,
     },
     Avatar: {
-      type: String,
-      default: "1",
+      type: Number,
+      default: 1,
     },
     Verified: {
       type: Boolean,
