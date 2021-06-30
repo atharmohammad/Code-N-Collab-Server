84cc247550f1fb643e1df450d471f7db4e79c11f
Adnan <adnanshamsi023@gmail.com>
2021-06-30 14:25:30 +0530

    exceptional handling in socket io event listener

--- src/socketFunc/Compile.js
+++ src/socketFunc/Compile.js
@@ -2,34 +2,37 @@ const { getUser } = require("../utils/Users");
 const { compilerFunc } = require("../Function/compilerFunc");
 
 module.exports = function (io) {
-  try{
-  io.on("connection", (socket) => {
-    socket.on("Compile_ON", ({ language, code, input, reason }) => {
-      const sids = io.of("/").adapter.sids;
-      const room = [...sids.get(socket.id)][1];
+  try {
+    io.on("connection", (socket) => {
+      socket.on("Compile_ON", ({ language, code, input, reason }) => {
+        try {
+          const sids = io.of("/").adapter.sids;
+          const room = [...sids.get(socket.id)][1];
 
-      if (!room) {
-        return;
-      }
+          if (!room) {
+            return;
+          }
 
-      if (reason === "code-editor")
-        socket.broadcast.to(room).emit("Compile_ON");
-
-      compilerFunc(language, code, input)
-        .then((res) => {
-         
           if (reason === "code-editor")
-            io.to(room).emit("COMPILE_OFF", res.data);
-          else io.to(socket.id).emit("COMPILE_OFF", res.data);
-        })
-        .catch((e) => {
-         
-          if (reason === "code-editor") io.to(room).emit("COMPILE_OFF", e.data);
-          else io.to(socket.id).emit("COMPILE_OFF", res.data);
-        });
+            socket.broadcast.to(room).emit("Compile_ON");
+
+          compilerFunc(language, code, input)
+            .then((res) => {
+              if (reason === "code-editor")
+                io.to(room).emit("COMPILE_OFF", res.data);
+              else io.to(socket.id).emit("COMPILE_OFF", res.data);
+            })
+            .catch((e) => {
+              if (reason === "code-editor")
+                io.to(room).emit("COMPILE_OFF", e.data);
+              else io.to(socket.id).emit("COMPILE_OFF", res.data);
+            });
+        } catch (e) {
+          console.log(e);
+        }
+      });
     });
-  });
-}catch(e){
-  console.log(e)
-}
+  } catch (e) {
+    console.log(e);
+  }
 };
